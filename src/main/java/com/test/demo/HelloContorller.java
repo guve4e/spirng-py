bedb71db927837496514ebb51a9cec732ea3c6c9
Valentin Kormanov <guve4e@gmail.com>
2019-02-18 21:57:34 -0500

    Adds more interesting controller

--- src/main/java/com/test/demo/HelloContorller.java
+++ src/main/java/com/test/demo/HelloContorller.java
@@ -1,13 +1,54 @@
 package com.test.demo;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+class SomeObject {
+    int id;
+    String description;
+    Date date;
+
+    public SomeObject(int i, String description, Date date) {
+        this.id = id;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
 
 @RestController
 public class HelloContorller {
 
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @ResponseBody
+    public SomeObject index() {
+        return new SomeObject(1001, "Shileto MATAFAKAAA", new Date());
     }
 }
